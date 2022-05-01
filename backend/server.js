6b039c838ff7a44f7963c8bc529804851ee2f38d
TimMTech <tmukhamedov97@gmail.com>
2022-05-01 15:52:07 -0400

    Modified heroku routes

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const app = express();
 const dotenv = require('dotenv')
 const routerUrls = require('./Routes/routes')
 const mongoose = require('mongoose')
+const path = require('path')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
+__dirname = path.resolve()
 
 dotenv.config()
 
@@ -16,11 +18,23 @@ mongoose.connect(
 
 app.use(express.json())
 app.use(cors())
+app.use("/app", routerUrls);
+
+//---------deployment--------//
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
+
+//----------deployment----------//
 
 
-app.use(express.static('frontend/build'))
 
-
-app.use('/app', routerUrls)
-app.listen(PORT, () => console.log(`Server Connected`))
+app.listen(PORT, () => console.log(`Server Connected On Port ${PORT}`))
 
