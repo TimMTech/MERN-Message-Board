394437b86b526de5877e2c0f53a355436e36c92a
TimMTech <tmukhamedov97@gmail.com>
2022-05-01 13:40:56 -0400

    Reverted Changes

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,10 @@ const app = express();
 const dotenv = require('dotenv')
 const routerUrls = require('./Routes/routes')
 const mongoose = require('mongoose')
-const path = require('path')
-const publicPath = path.join(__dirname, 'frontend', 'build')
+
 const port = process.env.PORT || 3000
 
-app.use(express.static(publicPath));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'))
-})
 
 dotenv.config()
 
